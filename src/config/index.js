1db9728a6c5edd1b825a49e1d92ac91f81bc16d7
Nandy <nandymandy09@hotmail.com>
2020-10-23 02:53:28 +0530

    minor changes

--- src/config/index.js
+++ src/config/index.js
@@ -3,11 +3,11 @@ import { config } from "dotenv";
 const { parsed } = config();
 
 export const {
+  DB,
   PORT,
   MODE,
   SECRET,
   BASE_URL,
-  IN_PROD = MODE !== "prod",
   URL = `${BASE_URL}`,
-  DB,
+  IN_PROD = MODE !== "prod",
 } = parsed;
