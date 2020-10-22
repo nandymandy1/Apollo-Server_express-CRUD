ab33f93791b1820988686388cb03561ae7dc0121
Nandy <nandymandy09@hotmail.com>
2020-10-23 02:55:58 +0530

    PORTS cahnged

--- src/config/index.js
+++ src/config/index.js
@@ -4,10 +4,11 @@ const { parsed } = config();
 
 export const {
   DB,
-  PORT,
   MODE,
   SECRET,
   BASE_URL,
   URL = `${BASE_URL}`,
   IN_PROD = MODE !== "prod",
 } = parsed;
+
+export const PORT = parsed.PORT || process.env.PORT;
